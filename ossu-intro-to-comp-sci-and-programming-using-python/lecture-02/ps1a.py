dc8d4013b61678c6a1cb57154946f6596977b0fb
AnimeshKumar923 <kumaranimesh923@gmail.com>
2022-11-30 02:20:22 +0530

    solve pset-01-part-a

---
+++ ossu-intro-to-comp-sci-and-programming-using-python/lecture-02/ps1a.py
@@ -0,0 +1,39 @@
+annual_salary=input('Enter your annual salary:')
+float_annual_salary=float(annual_salary)
+# print(float_annual_salary)
+
+portion_saved=input('Enter the percent of your salary to save, as a decimal:')
+float_portion_saved=float(portion_saved)
+# print(float_portion_saved)
+
+total_cost=input('Enter the cost of your dream home:')
+float_total_cost=float(total_cost)
+
+current_savings=0 #amount saved so far
+rate=0.04 #annual rate of investment return
+
+portion_down_payment=0.25*float_total_cost
+
+print('down payment is:', portion_down_payment)
+
+
+monthly_salary=float_annual_salary/12
+
+
+count=0
+
+while(current_savings<=portion_down_payment):
+
+    portion_saved=float_portion_saved*monthly_salary
+    # print('portion saved is:', portion_saved)
+
+    monthly_return=current_savings*rate/12
+    # print('monthly return is:', monthly_return)
+
+    current_savings=current_savings+monthly_return+portion_saved
+
+    # print('current saving is:', current_savings)
+
+    count=count+1
+
+print('number of months =', count)
